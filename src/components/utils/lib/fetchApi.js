8bf1f3a9fd4928e3d7b0bde01dc29aa02cee206e
GetmevaDev <devteam@getmeva.com>
2023-04-19 23:57:34 +0600

    fix

--- src/components/utils/lib/fetchApi.js
+++ src/components/utils/lib/fetchApi.js
@@ -1,7 +1,7 @@
 export async function fetchAPI(path) {
   const { API_URL } = process.env;
 
-  const response = await fetch(`${API_URL}${path}`, {
+  const response = await fetch(`http://localhost:1337/api/${path}`, {
     headers: {
       Authorization: `Bearer ${process.env.API_TOKEN}`,
     },
