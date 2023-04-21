774d2f6c07f1c2326e2e040784dc4fddae118ee6
GetmevaDev <devteam@getmeva.com>
2023-04-21 22:17:09 +0600

    logo

--- src/components/ui/Rgm/Rgm.jsx
+++ src/components/ui/Rgm/Rgm.jsx
@@ -1,15 +1,18 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 import styles from "./Rgm.module.scss";
 
 export const Rgm = () => (
   <div className={styles.rgm}>
-    <Image src="svg/rgm.svg" width={82} height={36} alt="rgm" />
+    <Link href="https://kaykovmedia.com/">
+      <Image src="/logo-2.svg" width={82} height={46} alt="rgm" />
+    </Link>
 
     <div className={styles.web}>
-      Web Design & Digital Marketing by{" "}
-      <p className={styles.robert}>Robert Gerov Media</p>
+      Web Design & Digital Marketing by
+      <p className={styles.robert}>Kaykov Media</p>
     </div>
   </div>
 );
