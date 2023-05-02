24bba80ab98231d6671f2cc7c11a082225bb72d8
GetmevaDev <devteam@getmeva.com>
2023-05-02 16:44:20 +0600

    google analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,3 +1,6 @@
+/* eslint-disable jsx-a11y/iframe-has-title */
+import Script from "next/script";
+
 import { DM_Serif_Display as DM, Jost } from "@next/font/google";
 
 import "swiper/css";
@@ -23,6 +26,44 @@ export const dm = DM({
 export default function App({ Component, pageProps }) {
   return (
     <main className={`${jost.variable} ${dm.variable}`}>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-45MF26K71Y"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-45MF26K71Y');
+        `}
+      </Script>
+
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+           (function (w, d, s, l, i) {
+              w[l] = w[l] || [];
+              w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
+              var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l != "dataLayer" ? "&l=" + l : "";
+              j.async = true;
+              j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
+              f.parentNode.insertBefore(j, f);
+            })(window, document, "script", "dataLayer", "GTM-NKFBQGB");
+        `}
+      </Script>
+
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-NKFBQGB"
+          height="0"
+          width="0"
+          style={{ display: "none", visibility: "hidden" }}
+        />
+      </noscript>
+
       <Component {...pageProps} />
     </main>
   );
