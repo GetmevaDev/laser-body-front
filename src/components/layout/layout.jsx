3f52d06625df9262a53684931d0c74ec99ad9c40
GetmevaDev <devteam@getmeva.com>
2023-05-02 12:32:57 +0600

    loader, fix faq

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PulseLoader from "react-spinners/PulseLoader";
 import useSWR from "swr";
 
 import Meta from "../seo/Meta";
@@ -9,6 +10,12 @@ import { Header } from "./Header/Header";
 import styles from "./layout.module.scss";
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+const override = {
+  display: "block",
+  margin: "0 auto",
+  borderColor: "red",
+};
 
 export const Layout = ({ children, title, description }) => {
   const {
@@ -21,7 +28,18 @@ export const Layout = ({ children, title, description }) => {
     fetcher
   );
 
-  if (isLoading) return <div>Loading...</div>;
+  if (isLoading) {
+    return (
+      <div className={styles.loader}>
+        <PulseLoader
+          color="#F000BB"
+          cssOverride={override}
+          size={30}
+          aria-label="Loading Spinner"
+        />
+      </div>
+    );
+  }
   if (isError) return <div>Error...</div>;
   return (
     <Meta title={title} description={description}>
