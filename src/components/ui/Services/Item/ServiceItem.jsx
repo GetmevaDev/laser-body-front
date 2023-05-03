0c7e9e01ff4db352c0d9c93c3a170694e3adb544
GetmevaDev <devteam@getmeva.com>
2023-05-03 19:14:44 +0600

    fix services

--- src/components/ui/Services/Item/ServiceItem.jsx
+++ src/components/ui/Services/Item/ServiceItem.jsx
@@ -1,17 +1,23 @@
 import Image from "next/image";
 import React from "react";
 
+import { Button } from "../../Button/Button";
+
 import styles from "./ServiceItem.module.scss";
 
 export const ServiceItem = ({ text, image }) => (
   <div className={styles.service}>
-    <Image
-      width={345}
-      height={330}
-      src={image?.data?.attributes?.url}
-      alt="services-item"
-      className={styles.image}
-    />
+    <div className={styles.image_inner}>
+      <Image
+        width={345}
+        height={330}
+        src={image?.data?.attributes?.url}
+        alt="services-item"
+        className={styles.image}
+      />
+    </div>
+
+    <Button className={styles.button}>shop now</Button>
 
     <div className={styles.name}>{text}</div>
   </div>
